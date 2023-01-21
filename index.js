9856a6ebdd6c1074db7022193abaa0100d958f0f
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-21 23:08:34 +0600

    fixed bugs

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ app.use("/assets",express.static(path.join(__dirname, 'public/assets')));
 //file storage
 
 const storage = multer.diskStorage({
-    destination: function (req,res,cb) {
+    destination: function (req,file,cb) {
         cb(null, "public/assets");
     },
-    filename: function(req,res,cb){
+    filename: function(req,file,cb){
         cb(null, file.originalname);
     }
 })
